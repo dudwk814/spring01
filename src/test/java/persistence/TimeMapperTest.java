0c9281e72c082c0012f87a3f4e2f5d757d880651
dudwk814 <kj99658103@gmail.com>
2020-09-16 16:46:53 +0900

    2020-09-16 mybatis setting

---
+++ src/test/java/persistence/TimeMapperTest.java
@@ -0,0 +1,30 @@
+package persistence;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import mapper.TimeMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j
+@ContextConfiguration("file:web/WEB-INF/applicationContext.xml")
+public class TimeMapperTest {
+
+    @Setter(onMethod_ = {@Autowired})
+    private TimeMapper timeMapper;
+
+    @Test
+    public void testGetTime() {
+        log.info(timeMapper.getClass().getName());
+        log.info(timeMapper.getTime());
+    }
+
+    @Test
+    public void testGetTime2() {
+        log.info(timeMapper.getTime2());
+    }
+}

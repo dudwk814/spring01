077d746112c8ce701c1b2da32c82216243786eea
dudwk814 <kj99658103@gmail.com>
2020-09-23 10:46:35 +0900

    2020-09-22 reply ajax test

--- web/WEB-INF/views/board/get.jsp
+++ web/WEB-INF/views/board/get.jsp
@@ -61,6 +61,56 @@
 </div>
 <script src="/resources/js/reply.js"></script>
 <script type="text/javascript">
+
+    console.log("==========================");
+    console.log("JS TEST");
+
+    var bnoValue = '<c:out value="${board.bno}"/>';
+
+  /*  // for replyService add test
+    replyService.add(
+        {reply:"JS Test", replyer:"tester", bno:bnoValue},
+        function (result) {
+            alert("REULST : " + result);
+        }
+    );*/
+
+    //reply List Test
+     /*replyService.getList({bno:bnoValue, page:1}, function(list){
+
+          for(var i = 0,  len = list.length||0; i < len; i++ ){
+            console.log(list[i]);
+          }
+    });*/
+
+    //13번 댓글 삭제 테스트
+    /*replyService.remove(13, function (count) {
+
+        console.log(count);
+
+        if (count == "success") {
+            alert("REMOVED");
+        }
+    }, function (err) {
+        alert('ERROR....');
+    });*/
+
+    //12번 댓글 수정 테스트
+    /*replyService.update({
+        rno: 12,
+        bno: bnoValue,
+        reply: "MODIFIED Reply......"
+    }, function (result) {
+        alert("수정 완료....");
+    });*/
+
+    // 12번 댓글 조회 테스트
+   /* replyService.get(12, function (data) {
+        console.log(data);
+    })*/
+
+</script>
+<script type="text/javascript">
     $(document).ready(function () {
         var operForm = $("#operForm");
 
--- web/resources/js/reply.js
+++ web/resources/js/reply.js
@@ -1,3 +1,107 @@
 console.log("Reply Module............");
 
-var replyService = {};
+var replyService = (function () {
+
+    // 댓글 등록 함수
+    function add(reply, callback, error) {
+        console.log("add reply..........");
+
+        $.ajax({
+           type : 'post',
+           url : '/replies/new',
+           data : JSON.stringify(reply),
+           contentType : "application/json; charset=utf-8",
+           success : function (result, status, xhr) {
+               if (callback) {
+                   callback(result);
+               }
+           },
+           error : function (xhr, status, er) {
+               if (error) {
+                   error(er);
+               }
+           }
+        });
+    }
+
+    //댓글 목록 함수
+    	function getList(param, callback, error) {
+
+		var bno = param.bno;
+		var page = param.page || 1;
+
+		$.getJSON("/replies/pages/" + bno + "/" + page + ".json",
+				function(data) {
+					if (callback) {
+						callback(data);
+					}
+				}).fail(function(xhr, status, err) {
+			if (error) {
+				error();
+			}
+		});
+	}
+
+	// 댓글 삭제 함수
+    function remove(rno, callback, error) {
+        $.ajax({
+            type: 'delete',
+            url: '/replies/' + rno,
+            success: function (deleteResult, status, xhr) {
+                if (callback) {
+                    callback(deleteResult);
+                }
+            },
+            error: function (xhr, status, er) {
+                if (error) {
+                    error(er);
+                }
+            }
+        });
+    }
+
+    //댓글 수정 함수
+    function update(reply, callback, error) {
+
+        console.log("RNO : " + reply.rno);
+
+        $.ajax({
+            type: 'put',
+            url: '/replies/' + reply.rno,
+            data: JSON.stringify(reply),
+            contentType: "application/json; charset=utf-8",
+            success: function (result, status, xhr) {
+                if (callback) {
+                    callback(result);
+                }
+            },
+            error: function (xhr, status, er) {
+                if (error) {
+                    error(er);
+                }
+            }
+        });
+    }
+
+    // 댓글 조회 함수
+    function get(rno, callback, error) {
+
+        $.get("/replies/" + rno + ".json", function (result) {
+            if (callback) {
+                callback(result);
+            }
+        }).fail(function (xhr, status, err) {
+            if (err) {
+                error();
+            }
+        });
+    }
+    return {
+        add : add,
+        getList : getList,
+        remove : remove,
+        update : update,
+        get: get
+    };
+
+})();

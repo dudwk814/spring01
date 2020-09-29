bf1a5f0f921408c6e7ce97bd4f1d5a2fec0454ef
dudwk814 <kj99658103@gmail.com>
2020-09-29 10:24:15 +0900

    2020-09-28 spring security CustomUserService

---
+++ src/main/java/security/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package security;
+
+import domain.CustomUser;
+import domain.MemberVO;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import mapper.MemberMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Log4j
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Setter(onMethod_ = {@Autowired})
+    private MemberMapper memberMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        log.warn("Load user By UserName : " + userName);
+
+        // userName means userid
+        MemberVO vo = memberMapper.read(userName);
+
+        log.warn("queried by member mapper : " + vo);
+
+        return vo == null ? null : new CustomUser(vo);
+    }
+}
